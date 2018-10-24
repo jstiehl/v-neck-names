d2d6c14d0822b726572fdfc5bdeb54193bec77c6
James Stiehl <jstiehl@gmail.com>
2018-10-24 15:26:48 -0700

    name generator

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png"><span class="img-text">andelay</span>
-    <HelloWorld msg="Industries"/>
+    <h1>V-neck LLC Name Generator</h1>
+    <div v-if="showName" class="company-name">
+      <div class="img-wrapper">
+        <img alt="Vue logo" src="./assets/logo.png"><span class="img-text">{{ stripped }}</span>
+      </div>
+      <HelloWorld msg="LLC"/>
+    </div>
+    <div v-else="showName">
+      <label>LLC Name</label><br />
+      <input v-model="companyName" type="text" placeholder="Enter desired LLC Name..." /><br />
+      <button @click.prevent="toggleInput">Generate LLC Name</button>
+    </div>
+    <button @click.prevent="toggleInput" v-if="showName">Enter New Name</button>
   </div>
 </template>
 
@@ -10,8 +21,23 @@ import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
+  data: () => (
+  {
+      companyName: "",
+      showName: false
+  }),
   components: {
     HelloWorld
+  },
+  computed: {
+    stripped() {
+      return this.companyName.split('').filter((l,i) => i != 0).join('')
+    }
+  },
+  methods: {
+    toggleInput(e) {
+      this.showName = !this.showName
+    }
   }
 }
 </script>
@@ -24,12 +50,30 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  width: 80%;
+  margin-right: auto;
+  margin-left: auto;
+  min-height: 500px;
+  border: 1px solid black;
+  border-radius: 25px;
+}
+
+.img-wrapper {
+  display: flex;
+  margin: auto;
+  justify-content: center;
 }
 
 .img-text {
-  position: absolute;
-  top:220px;
-  left: 710px;
   font-size: 25px;
+  align-self: flex-end;
+  margin-left: -75px;
+  margin-bottom: 3px;
+}
+
+.company-name {
+  display: grid;
+  grid-template-columns: 100%;
+  align-content: center;
 }
 </style>
